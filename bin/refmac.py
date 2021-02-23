a3a84bd240ca22ed3c1d64b2668c2030d7781274
Jonathan Davies <jonathanrd@gmail.com>
2021-02-23 13:41:08 +0100

    code lint

--- bin/refmac.py
+++ bin/refmac.py
@@ -3,7 +3,10 @@
 class refmac:
     ''' Refmac5 Wrapper '''
 
-    def __init__(self,pdb, mtz, mode = "HKRF", cycles = 10,bref = "ISOT",weight = None, showcommand=False, outputfilename = None, coot=False, breset=None, verbose=False, custom=None, libin=None, tlsin=None, labeltype="normal", logview=None):
+    def __init__(self, pdb, mtz, mode = "HKRF", cycles = 10,
+    bref = "ISOT", weight = None, showcommand = False, outputfilename = None,
+    coot = False, breset = None, verbose = False, custom = None, libin = None,
+    tlsin = None, labeltype = "normal", logview = None):
 
         # Generate timestamp
         import datetime
@@ -27,23 +30,23 @@ class refmac:
         self.logview = logview
 
         # Output filename, use timestamp if not set
-        if (outputfilename):
+        if outputfilename:
             self.outputfilename = outputfilename
         else:
-            self.outputfilename = timestamp+"-refmac"
+            self.outputfilename = timestamp + "-refmac"
 
 
         # Run mtzinfo to get list of column headers
         import subprocess,re
-        s = subprocess.check_output("mtzinfo "+self.mtz, shell=True)
+        s = subprocess.check_output("mtzinfo " + self.mtz, shell = True)
         for line in s.decode('ascii').split("\n"):
-            if (re.match("^LABELS.*", line)):
+            if re.match("^LABELS.*", line):
                 self.labels = line.split()[1:]
 
         # Using common free r labels, find and set the Free R label
         default_free = ['FREE', 'RFREE', 'FREER', 'FreeR_flag']
         try:
-            self.label_free = "FREE="+[i for i in self.labels if i in default_free][0]
+            self.label_free = "FREE=" + [i for i in self.labels if i in default_free][0]
         except:
             self.label_free = ""
 
@@ -82,66 +85,72 @@ class refmac:
         cmd += f"ncyc {self.cycles}\n"
 
         # Set the input labels
-        if (self.labeltype == "normal"):
+        if self.labeltype == "normal":
             cmd += f"labin  {self.label_fp} {self.label_sigfp} {self.label_free}\n"
-        elif (self.labeltype == "sad"):
+
+        elif self.labeltype == "sad":
             cmd += f"labin  {self.label_fplus} {self.label_sigfplus} {self.label_fminus} {self.label_sigfminus} {self.label_free}\n"
-        if (self.labeltype == "hl"):
+
+        if self.labeltype == "hl":
             cmd += f"labin  {self.label_fp} {self.label_sigfp} HLA=HLA HLB=HLB HLC=HLC HLD=HLD {self.label_free}\n"
 
         # Have the B factors been reset?
-        if (self.breset != None): cmd+= "BFACTOR SET " + str(self.breset) +"\n"
+        if self.breset != None:
+            cmd+= "BFACTOR SET " + str(self.breset) + "\n"
 
         # Type of B Factor refinement to run
-        if (self.bref != "ISOT"): cmd+= "REFI BREF " + str(self.bref) +"\n"
+        if self.bref != "ISOT":
+            cmd+= "REFI BREF " + str(self.bref) + "\n"
 
         # Mode of refinement
-        if (self.mode != "HKRF"): cmd+= "MODE " + str(self.mode) +"\n"
+        if self.mode != "HKRF":
+            cmd+= "MODE " + str(self.mode) + "\n"
 
         # Adjust the restraints weight
-        if (self.weight): cmd+= "WEIGHT MATRIX " + str(self.weight) +"\n"
+        if self.weight:
+            cmd+= "WEIGHT MATRIX " + str(self.weight) + "\n"
 
         # Any extra custom keywords?
-        if (self.custom): cmd+= str(self.custom) +"\n"
+        if self.custom:
+            cmd+= str(self.custom) + "\n"
 
 
         # End the command entry
-        cmd += ("NOHARVEST\n"
-        "END\n"
-        "eof")
+        cmd += "NOHARVEST\nEND\neof"
 
 
 
         # Print the final command to terminal
-        if (self.showcommand == True):
+        if self.showcommand == True:
             print(cmd)
             import sys
             sys.exit()
 
         # Start the log file
         import sys
-        log = open(self.outputfilename+".log", "w")
+        log = open(self.outputfilename + ".log", "w")
         log.write("Refmac run through python wrapper using command:\n\n")
-        log.write("refmac.py "+(" ".join(sys.argv[1:]))+"\n\n")
+        log.write("refmac.py " + (" ".join(sys.argv[1:])) + "\n\n")
         log.close()
 
         # Show logview?
-        if (self.logview):
-            subprocess.Popen(["logview",self.outputfilename+".log"],stdout=subprocess.DEVNULL,stderr=subprocess.DEVNULL)
+        if self.logview:
+            subprocess.Popen(["logview", self.outputfilename + ".log"],
+            stdout = subprocess.DEVNULL, stderr = subprocess.DEVNULL)
 
         # Print to terminal
-        if (self.verbose):
+        if self.verbose:
             print("Running Refmac..")
-            print("Log file at: "+self.outputfilename+".log")
+            print("Log file at: " + self.outputfilename + ".log")
 
         # Run the command
-        s = subprocess.check_output(cmd, shell=True)
+        s = subprocess.check_output(cmd, shell = True)
         self.result = s.decode('ascii')
 
         # Output the final refinement stats to the terminal
-        if (self.verbose):
+        if self.verbose:
             import re
-            shakes = open(self.outputfilename+".log", "r")
+            shakes = open(self.outputfilename + ".log", "r")
             for line in shakes:
                 if re.match("           R factor    .+", line):
                     print ("       "+line.strip())
@@ -152,94 +161,97 @@ class refmac:
                 if re.match("      Rms BondAngle    .+", line):
                     print ("  "+line.strip())
 
-            print("coot --pdb "+self.outputfilename+".pdb --auto "+self.outputfilename+".mtz")
+            print("coot --pdb", self.outputfilename + ".pdb --auto",
+            self.outputfilename+".mtz")
 
         # Open the output files in Coot
-        if (self.coot == True):
+        if self.coot:
             import subprocess
-            subprocess.check_output("coot --pdb "+self.outputfilename+".pdb --auto "+self.outputfilename+".mtz &>/dev/null", shell=True)
+            subprocess.check_output("coot --pdb " + self.outputfilename+".pdb --auto " + self.outputfilename+".mtz &>/dev/null", shell=True)
 
 
 
 import argparse
 
-parser = argparse.ArgumentParser(prog='refmac.py', usage='%(prog)s [options]')
+parser = argparse.ArgumentParser(prog = 'refmac.py',
+                                 usage = '%(prog)s [options]')
 
 optional = parser._action_groups.pop()
 required = parser.add_argument_group('required arguments')
 
-required.add_argument("--pdb", metavar="input.pdb",
-                    type=ascii,
-                    required=True,
-                    help="PDB input file")
+required.add_argument("--pdb", metavar = "input.pdb",
+                    type = ascii,
+                    required = True,
+                    help = "PDB input file")
 
-required.add_argument("--mtz", metavar="input.mtz",
-                    type=ascii,
-                    required=True,
-                    help="MTZ input file")
+required.add_argument("--mtz", metavar = "input.mtz",
+                    type = ascii,
+                    required = True,
+                    help = "MTZ input file")
 
 
-optional.add_argument("--cycles", metavar="10",
-                    help="No. of cycles (Default: 10)",
-                    type=int, default=10)
+optional.add_argument("--cycles", metavar = "10",
+                    help = "No. of cycles [10]",
+                    type = int, default=10)
 
-optional.add_argument("--breset", metavar="30",
-                    help="Reset B Factor at start to value",
-                    type=int, default=None)
+optional.add_argument("--breset", metavar = "30",
+                    help = "Reset B Factor at start to specified value",
+                    type = int, default = None)
 
-optional.add_argument("--bref",  metavar="ISOT",
-                    help="B refinement (Default: ISOT. Options: OVER, ISOT, ANIS, MIXED)",
-                    type=str,
-                    choices=['OVER', 'ISOT', 'ANIS', 'MIXED'],
+optional.add_argument("--bref",  metavar = "ISOT",
+                    help = "B refinement (OVER, [ISOT], ANIS, MIXED)",
+                    type = str,
+                    choices = ['OVER', 'ISOT', 'ANIS', 'MIXED'],
                     default = 'ISOT')
 
-optional.add_argument("--mode", metavar="HKRF",
-                    help="Refinement Mode (Default: HKRF. Options: HKRF, RIGID, TLSR)",
-                    type=str,
-                    default="HKRF",
+optional.add_argument("--mode", metavar = "HKRF",
+                    help = "Refinement Mode ([HKRF], RIGID, TLSR)",
+                    type = str,
+                    default = "HKRF",
                     choices = ['HKRF', 'RIGID', 'TLSR'])
 
-optional.add_argument("--labels", metavar="normal",
-                    help="Do you want to use SAD or experimental phasing data? (Default: normal. Options: normal, sad, hl)",
-                    type=str,
-                    default="normal",
+optional.add_argument("--labels", metavar = "normal",
+                    help = "Refine with SAD or exp. data ([normal], sad, hl)",
+                    type = str,
+                    default = "normal",
                     choices = ['normal', 'sad', 'hl'])
 
 optional.add_argument("--weight", metavar="0.5",
-                    help="Weight matrix (Default: Auto)",
-                    type=float,
+                    help = "Weight matrix [auto]",
+                    type = float,
                     default = None) # Leave None for auto
 
-optional.add_argument("--output", metavar="",
-                    help="Outfile file name (Default: YYMMDD-HHMMSS-refmac)",
-                    type=str,
+optional.add_argument("--output", metavar = "",
+                    help = "Outfile file name [YYMMDD-HHMMSS-refmac]",
+                    type = str,
                     default = None)
 
 optional.add_argument("--showcommand",
-                    help="Print the full refmac command and stop.",
-                    action="store_true")
+                    help = "Print the full refmac command and stop.",
+                    action = "store_true")
 
 optional.add_argument("--logview",
-                    help="Run CCP4 logview while refmac is running",
-                    action="store_true")
+                    help = "Run CCP4 logview while refmac is running.",
+                    action = "store_true")
 
 optional.add_argument("--coot",
-                    help="Run Coot after refinement",
-                    action="store_true")
+                    help = "Run Coot after refinement.",
+                    action = "store_true")
 
 optional.add_argument("--custom",
-                    help="Pass custom keywords to refmac",
-                    type=str, default = None)
+                    help = "Pass custom keywords to refmac.",
+                    type = str, default = None)
 
 optional.add_argument("--libin",
-                    help="Add a dictionary",
-                    type=str, default = None)
+                    help = "Add a dictionary.",
+                    type = str, default = None)
 
 optional.add_argument("--tlsin",
-                    help="TLS definitions",
-                    type=str, default = None)
+                    help = "TLS definitions.",
+                    type = str, default = None)
 
-optional.add_argument("-v","--verbose", help="Verbose", action="store_true")
+optional.add_argument("-v", "--verbose",
+                    help = "Verbose", action = "store_true")
 
 parser._action_groups.append(optional)
 
@@ -250,7 +262,12 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     # Pass args to the main class
-    program = refmac(pdb = args.pdb, cycles = args.cycles, mtz = args.mtz, bref=args.bref, weight=args.weight, showcommand=args.showcommand, mode=args.mode, outputfilename=args.output, coot=args.coot, breset=args.breset, verbose=args.verbose, libin=args.libin, tlsin=args.tlsin, labeltype=args.labels, logview=args.logview, custom=args.custom)
+    program = refmac(pdb = args.pdb, cycles = args.cycles, mtz = args.mtz,
+    bref = args.bref, weight = args.weight, showcommand = args.showcommand,
+    mode = args.mode, outputfilename = args.output, coot = args.coot,
+    breset = args.breset, verbose = args.verbose, libin = args.libin,
+    tlsin = args.tlsin, labeltype = args.labels, logview = args.logview,
+    custom = args.custom)
 
     # Run the main class
     program.run()
